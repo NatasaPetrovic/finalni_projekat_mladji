6003926fa88650f36bb66dfa6e52cfa030af01ff
br0k3n <filipskemba@gmail.com>
2016-04-10 07:03:12 +0000

    Dodat template (header.html) i glavna stranica (index.php)

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include "header.html"
+?>
+<head>
+<link rel="icon" href="http://megaicons.net/static/img/icons_sizes/66/163/16/under-construction-icon.png" type="image/png">
+<title>U konstrukciji</title>
+</head>
+<body>
+    <div class="container">
+        <img src="http://i.imgur.com/tlO3m6G.png" />
+        <h2>Website je trenutno u konstrukciji, dodjite opet kasnije.</h2>
+    </div>
+</body>
+</html>
