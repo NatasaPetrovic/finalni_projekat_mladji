45eca219876df148d099849d82127ee4eb03364f
br0k3n <filipskemba@gmail.com>
2016-04-10 07:28:30 +0000

    Dodat template (header.html) i pocetna stranica (index.php)

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 include "header.html"
 ?>
 <head>
-<link rel="icon" href="http://megaicons.net/static/img/icons_sizes/66/163/16/under-construction-icon.png" type="image/png">
 <title>U konstrukciji</title>
 </head>
 <body>
