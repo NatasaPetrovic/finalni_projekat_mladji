a53d8c6ec7f2bdd2fff2a923b2c864421c0f7a9e
br0k3n <filipskemba@gmail.com>
2016-04-17 06:51:36 +0000

    Doradjena glavna strana

--- index.php
+++ index.php
@@ -2,16 +2,60 @@
 include "header.html"
 ?>
 <head>
-<<<<<<< HEAD
-<link rel="icon" href="http://megaicons.net/static/img/icons_sizes/66/163/16/under-construction-icon.png" type="image/png">
-=======
->>>>>>> eks-di-di-di
 <title>U konstrukciji</title>
+<link rel="stylesheet" href="cssmain.css">
 </head>
 <body>
     <div class="container">
-        <img src="http://i.imgur.com/tlO3m6G.png" />
-        <h2>Website je trenutno u konstrukciji, dodjite opet kasnije.</h2>
+        <div class="jumbotron">
+        <!--<img src="http://i.imgur.com/tlO3m6G.png" />
+        <h2>Website je trenutno u konstrukciji, dodjite opet kasnije.</h2>-->
+        <center>
+	<div>
+	<h1>O našem projektu</h1>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	</div>
+	
+	<h1>Ko su članovi grupe</h1>
+    
+<br>    <div class="row">
+        
+        <div class="col-lg-4"> 
+            
+            <img src="images/users/filip.jpg" class="lala" style="border-radius:100px;">
+            <h3>Filip Škembarević</h3>
+            <br>
+			
+        </div>
+        
+        <div class="col-lg-4">
+              
+            <img src="images/users/nadja.jpg" class="lala" style="border-radius:100px;">
+            <h3>Nađa Živković</h3>
+           <br>
+		   
+            </div>
+        
+                
+        <div class="col-lg-4" width="100">
+              
+            <img src="images/users/nikola.jpg" class="lala" style="border-radius:100px;">
+            <h3>Nikola Ignjatović</h3>
+           <br>
+		   
+            </div>
+        
+    
+	<br>
+	<br>
+	
+	
+	
+        </div>
+    </center>
+    </div>
     </div>
 </body>
 </html>
