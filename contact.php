4df5a685a79cab31b80ae3ed4d797086e72986e6
bajaizresnika <mihajlo.trumic@gmail.com>
2016-04-24 07:43:28 +0000

    contact brae

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+include "header.html"
+?>
+<head>
+<title>U konstrukciji</title>
+<link rel="stylesheet" href="cssmain.css">
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+        <form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="message" class="col-sm-2 control-label">Poruka</label>
+    <div class="col-sm-6">
+        <textarea name="message" rows = "4" class ="form-control"></textarea>
+    </div>
+  </div>
+  
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Pošalji</button>
+    </div>
+  </div>
+</form>
+       
+	</div>
+    </div>
+    <footer id="foteraja">
+        &copy;copyright 2016 , Created by our team
+    </footer>
+    </div>
+</body>
+</html>
