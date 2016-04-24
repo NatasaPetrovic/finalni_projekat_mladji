b13e97023e6bbd6f9bc1930636b86757c291b9c9
ngxalpha <nikola.ignjatovic5702@gmail.com>
2016-04-24 08:01:28 +0000

    slicice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(".clanovi").hide();
     
     $(".imgavfave").click(function() {
-       $clan = $(this).attr("id").substr(10,1);
+       $clan = $(this).attr("id").substr(10,2);
        $(".clanovi").hide();
        $(".clanovi[value=" + $clan + "]").show();
     });
